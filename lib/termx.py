105000d6ed83d6dda75b55f565b4a6f838127fb7
D3VIL-1 <98472041+D3VIL-1@users.noreply.github.com>
2022-09-04 20:47:40 +0800

    Add files via upload

---
+++ lib/termx.py
@@ -0,0 +1,18 @@
+import os
+import time
+# Set color
+G = '\033[1;32m' # Green
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.003)
+delay_print
+delay_print (""+G+"  ████████╗██████╗ ██████╗ ███╗   ███╗██╗   ██╗██╗  ██╗""\n")
+delay_print (""+G+"  ╚══██╔══╝╚═══██║ ██╔══██╗████╗ ████║██║   ██║╚██╗██╔╝""\n")
+delay_print (""+G+"     ██║    █████║ ██████╔╝██╔████╔██║██║   ██║ ╚███╔╝""\n")
+delay_print (""+G+"     ██║    ╚══██║ ██╔══██╗██║╚██╔╝██║██║   ██║ ██╔██╗""\n")
+delay_print (""+G+"     ██║   ██████║ ██║  ██║██║ ╚═╝ ██║╚██████╔╝██╔╝ ██╗""\n")
+delay_print (""+G+"     ╚═╝   ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝""\n")
+delay_print (""+G+" <═══════════════Created By 'MrHacker-X'═══════════════>""\n")
